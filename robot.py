81a926688626d9ec156d732c0fbef4d1d2efc3dd
nazanin1369 <nazanin1427@gmail.com>
2017-09-04 15:45:39 -0700

    particle filter logic

--- robot.py
+++ robot.py
@@ -124,3 +124,52 @@ myrobot = myrobot.move(-pi/2, 15.0)
 print myrobot.sense()
 myrobot = myrobot.move(-pi/2, 10.0)
 print myrobot.sense()
+
+
+####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
+N = 1000
+p = []
+T = 10
+myrobot = robot()
+
+for t in range(T):
+        myrobot = myrobot.move(0.1, 5.0)
+        Z = myrobot.sense()
+
+        # Resampling
+        for i in range(N):
+        x = robot()
+        x.set_noise(0.05, 0.05, 5.0)
+        p.append(x)
+
+        p2 = []
+        for i in range(N):
+        p2.append(p[i].move(0.1, 5.0))
+        p = p2
+
+        w = []
+        #Setting importance weights
+        for i in range(N):
+                w.append(p[i].measurement_prob(Z)
+        print w
+
+        # You should make sure that p3 contains a list with particles
+        # resampled according to their weights.
+        # Also, DO NOT MODIFY p.
+
+        p3 = []
+        index = int(random.random() * N)
+        beta = 0.0
+        mw = max(w)
+
+        for i in range(N):
+                beta += random.random() * 2.0 * mw;
+                while beta >w[index]:
+                        beta -= w[index]
+                        index = (index + 1) % N
+                p3.append(p[index])
+
+        p = p3
+        #print p
+        print eval(myrobot, p)
+
